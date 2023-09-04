06754bff50e49e4a2c811b18edd86474329d7c8c
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-04 11:09:55 -0700

    ✅ Pass tests

--- R/validate_baits.R
+++ R/validate_baits.R
@@ -1,3 +1,13 @@
+validate_unique_bait_per_line <- function(lines_baits) {
+  number_of_lines <- lines_baits$Linea |>
+    unique() |>
+    length()
+  number_of_unique_lines_and_baits <- lines_baits |>
+    unique() |>
+    nrow()
+  number_of_lines == number_of_unique_lines_and_baits
+}
+
 get_line_and_atrayente <- function(ig_posicion) {
   ig_posicion |>
     dplyr::select("Linea", "Atrayente")
