71815de33dcdcfedfe2fc82b23f69bc0969aa1e0
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-11 14:41:04 -0700

    ♻️  Refactor

--- R/validates_id_k9.R
+++ R/validates_id_k9.R
@@ -1,3 +1,14 @@
+#' @export
+check_traces <- function(k9_traces_path = "data/esfuerzos_k9_gatos_guadalupe/registros_rastros_de_gatos_k9_guadalupe.csv") {
+  data_traces <- readr::read_csv(k9_traces_path, show_col_types = FALSE)
+  config <- list(
+    id_column_name = "ID_punto",
+    variable = "Tipo_de_rastro",
+    message_for_id_and_zones = "have different traces"
+  )
+  stop_if_variable_in_id_and_variable_column_are_wrong(data_traces, config)
+}
+
 split_id <- function(id_punto) {
   if (is.na(id_punto)) {
     return(rep(NA, 4))
@@ -8,15 +19,4 @@ split_id <- function(id_punto) {
 get_trace_from_id <- function(dataframe, id_column_name) {
   splited_ids <- sapply(dataframe[[id_column_name]], split_id, simplify = FALSE, USE.NAMES = FALSE)
   sapply(splited_ids, `[[`, 4)
-}
-
-#' @export
-check_traces <- function(k9_traces_path = "data/esfuerzos_k9_gatos_guadalupe/registros_rastros_de_gatos_k9_guadalupe.csv") {
-  data_traces <- readr::read_csv(k9_traces_path, show_col_types = FALSE)
-  config <- list(
-    id_column_name = "ID_punto",
-    variable = "Tipo_de_rastro",
-    message_for_id_and_zones = "have different traces"
-  )
-  stop_if_variable_in_id_and_variable_column_are_wrong(data_traces, config)
 }
--- tests/testthat/test-camaras_trampa.R
+++ tests/testthat/test-camaras_trampa.R
@@ -3,8 +3,8 @@ describe("ID is consisitent with zone", {
   camaras_trampa <- readr::read_csv(correct_guadalupe_camaras_path, show_col_types = FALSE)
   wrong_guadalupe_camaras_path <- "/workdir/tests/data/wrong_camaras_trampa_gatos_isla_guadalupe_revision_campo.csv"
   it("Check ID's and zones consistency in Guadalupe", {
-    expect_no_error(check_id_and_zones_guadalupe(correct_guadalupe_camaras_path))
-    expect_error(check_id_and_zones_guadalupe(wrong_guadalupe_camaras_path), "🚨 Rows 2, 13, and 21 have different zones in ID")
+    expect_no_error(check_cameras_id_and_zones_guadalupe(correct_guadalupe_camaras_path))
+    expect_error(check_cameras_id_and_zones_guadalupe(wrong_guadalupe_camaras_path), "🚨 Rows 2, 13, and 21 have different zones in ID")
   })
   it("ID is consistent", {
     id_column <- "ID_camara_trampa"
