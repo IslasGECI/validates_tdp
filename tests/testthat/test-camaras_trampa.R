522edfcdf23069fe8cfc9c4e24a4cd1d6c5812e0
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-11 12:08:14 -0700

    🛑🧪 Fail tests

--- tests/testthat/test-camaras_trampa.R
+++ tests/testthat/test-camaras_trampa.R
@@ -1,8 +1,10 @@
 describe("ID is consisitent with zone", {
   correct_guadalupe_camaras_path <- "/workdir/tests/data/camaras_trampa_gatos_isla_guadalupe_revision_campo.csv"
   camaras_trampa <- readr::read_csv(correct_guadalupe_camaras_path, show_col_types = FALSE)
+  wrong_guadalupe_camaras_path <- "/workdir/tests/data/wrong_camaras_trampa_gatos_isla_guadalupe_revision_campo.csv"
   it("Check ID's and zones consistency in Guadalupe", {
     expect_no_error(check_id_and_zones_guadalupe(correct_guadalupe_camaras_path))
+    expect_error(check_id_and_zones_guadalupe(wrong_guadalupe_camaras_path), "🚨 Rows 2, 13, and 21 have different zones in ID")
   })
   it("ID is consistent", {
     id_column <- "ID_camara_trampa"
