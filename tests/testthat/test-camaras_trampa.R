6c1dfd46a3b5f83743901dec3c6c357e5e8b4759
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-11 11:45:19 -0700

    ♻️  Refactor

--- tests/testthat/test-camaras_trampa.R
+++ tests/testthat/test-camaras_trampa.R
@@ -1,7 +1,8 @@
 describe("ID is consisitent with zone", {
   camaras_trampa <- readr::read_csv("/workdir/tests/data/camaras_trampa_gatos_isla_guadalupe_revision_campo.csv", show_col_types = FALSE)
   it("ID is consistent", {
-    obtained <- is_id_consistent_with_zone(camaras_trampa)
+    id_column <- "ID_camara_trampa"
+    obtained <- is_id_consistent_with_zone(camaras_trampa, id_column)
     expect_true(obtained)
   })
 })
