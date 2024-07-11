9d6acac38d32e49f8773da0c615e54ad0e3aef29
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-11 11:43:40 -0700

    ♻️  Refactor

--- R/check_id_and_zone.R
+++ R/check_id_and_zone.R
@@ -1,5 +1,6 @@
 is_id_consistent_with_zone <- function(camaras_trampa) {
-  zones_from_id <- strtoi(str_match(camaras_trampa$ID_camara_trampa, "-0([1-8])-")[, 2])
+  id_column_name <- "ID_camara_trampa"
+  zones_from_id <- strtoi(str_match(camaras_trampa[[id_column_name]], "-0([1-8])-")[, 2])
   is_each_id_consistent_with_zone <- zones_from_id == camaras_trampa$Zona
   return(all(is_each_id_consistent_with_zone))
 }
--- tests/testthat/test-camaras_trampa.R
+++ tests/testthat/test-camaras_trampa.R
@@ -1,8 +1,5 @@
-library("tidyverse")
-
-camaras_trampa <- read_csv("/workdir/tests/data/camaras_trampa_gatos_isla_guadalupe_revision_campo.csv", show_col_types = FALSE)
-
 describe("ID is consisitent with zone", {
+  camaras_trampa <- readr::read_csv("/workdir/tests/data/camaras_trampa_gatos_isla_guadalupe_revision_campo.csv", show_col_types = FALSE)
   it("ID is consistent", {
     obtained <- is_id_consistent_with_zone(camaras_trampa)
     expect_true(obtained)
