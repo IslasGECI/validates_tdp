4092b4cb2efaf2e38b33a85ff3d9e16a08b1a7a8
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-11 12:53:04 -0700

    ✅ Pass tests

--- R/check_id_and_zone.R
+++ R/check_id_and_zone.R
@@ -1,9 +1,14 @@
 check_id_and_zones_guadalupe <- function(cameras_path) {
   cameras_data <- readr::read_csv(cameras_path, show_col_types = FALSE)
-  if (!is_id_consistent_with_zone(cameras_data, "ID_camara_trampa")) {
-    return("error")
+  id_column_name <- "ID_camara_trampa"
+  zones_from_id <- strtoi(stringr::str_match(cameras_data[[id_column_name]], "-0([1-8])-")[, 2])
+  no_equal_indexes <- which(zones_from_id != cameras_data$Zona)
+  different_rows <- glue::glue_collapse(no_equal_indexes, ", ", last = " and ")
+  print(different_rows)
+  is_different <- length(no_equal_indexes) > 0
+  if (is_different) {
+    stop(glue::glue("🚨 Rows {different_rows} have different zones in ID"))
   }
-  return("no error")
 }
 
 is_id_consistent_with_zone <- function(camaras_trampa, id_column_name) {
