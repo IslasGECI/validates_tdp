abe6ed5073e1081c8486756301f80808e7cca284
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-11 13:34:59 -0700

    ♻️  Refactor

--- R/check_id_and_zone.R
+++ R/check_id_and_zone.R
@@ -1,10 +1,6 @@
 check_id_and_zones_guadalupe <- function(cameras_path) {
   cameras_data <- readr::read_csv(cameras_path, show_col_types = FALSE)
-  id_column_name <- "ID_camara_trampa"
-  zones_from_id <- get_zones_from_id(cameras_data, id_column_name)
-  no_equal_indexes <- which(zones_from_id != cameras_data$Zona)
-  message_for_id_and_zones <- "have different zones in ID"
-  stop_with_message(no_equal_indexes, message_for_id_and_zones)
+  stop_if_id_camaras_are_wrong(cameras_data)
 }
 
 stop_if_id_camaras_are_wrong <- function(cameras_data) {
