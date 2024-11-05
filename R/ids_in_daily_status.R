9f3e1e17b9ffab3a41e057833cfad3019a310d82
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-11-05 10:49:01 -0800

    ✅ Pass tests

--- R/ids_in_daily_status.R
+++ R/ids_in_daily_status.R
@@ -1,6 +1,6 @@
 check_all_ids_in_list_are_in_daily_status <- function(daily_status_path, traps_list_path) {
   daily_status <- readr::read_csv(daily_status_path)
   traps_list <- readr::read_csv(traps_list_path)
-  are_all_ids <- all(daily_status$ID_de_trampa %in% traps_list$ID)
+  are_all_ids <- all(traps_list$ID %in% daily_status$ID_de_trampa)
   return(are_all_ids)
 }
