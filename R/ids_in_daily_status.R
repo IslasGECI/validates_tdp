ca9717b3f17c35075355585fdf972c7ac20b63d2
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-11-05 12:44:06 -0800

    ♻️  Refactor

--- R/ids_in_daily_status.R
+++ R/ids_in_daily_status.R
@@ -2,7 +2,6 @@ check_all_ids_in_list_are_in_daily_status <- function(daily_status_path, traps_l
   daily_status <- readr::read_csv(daily_status_path)
   traps_list <- readr::read_csv(traps_list_path)
   are_all_ids <- .are_all_ids(traps_list, daily_status)
-  print(are_all_ids)
   should_stop(are_all_ids)
 }
 
@@ -17,8 +16,7 @@ should_stop <- function(are_all_ids) {
 
 .are_all_ids <- function(traps_list, daily_status) {
   condition <- traps_list$ID %in% daily_status$ID_de_trampa
-  are_all_ids <- list(is_right = all(condition), extra_ids = traps_list |>
-    filter(!condition))
+  are_all_ids <- list(is_right = all(condition), extra_ids = .extract_extra_ids(traps_list, daily_status))
   return(are_all_ids)
 }
 
