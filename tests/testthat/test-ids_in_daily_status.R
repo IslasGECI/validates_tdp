009c5bac7e1167061cf6a36abdeae799be130ba4
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-11-05 10:48:16 -0800

    🛑🧪 Fail tests

--- tests/testthat/test-ids_in_daily_status.R
+++ tests/testthat/test-ids_in_daily_status.R
@@ -1,10 +1,10 @@
 describe("All IDs defined in list are in daily status", {
   correct_daily_status_path <- "/workdir/tests/data/correct_daily_status.csv"
-  incorrect_daily_status_path <- "/workdir/tests/data/incorrect_daily_status.csv"
   traps_list_path <- "/workdir/tests/data/traps_ids.csv"
+  incorrect_traps_list_path <- "/workdir/tests/data/incorrect_traps_ids.csv"
 
   it("Check all ID's are in ids list", {
     expect_true(check_all_ids_in_list_are_in_daily_status(correct_daily_status_path, traps_list_path))
-    expect_false(check_all_ids_in_list_are_in_daily_status(incorrect_daily_status_path, traps_list_path))
+    expect_false(check_all_ids_in_list_are_in_daily_status(correct_daily_status_path, incorrect_traps_list_path))
   })
 })
